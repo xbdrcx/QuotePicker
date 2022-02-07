09df9c0137529cc7d83d760e087b2ff46aedc483
Bruno Cruz <cruzzz.1998@live.com.pt>
2022-02-07 20:34:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ soup = BeautifulSoup(page.content, "html.parser")
 quotes = soup.find_all(style="display: flex;justify-content: space-between")
 authors = soup.find_all(title="view author")
 
-numQuotes = len(quotes)
-
-pick = randrange(numQuotes)
+pick = randrange(len(quotes))
 
 print("\n", quotes[pick].text, "\n - ", authors[pick].text, "\n\n\n")
 
